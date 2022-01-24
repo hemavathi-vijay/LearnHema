6a312001c56dcf0d6bd57f8c9ae84835f3a9b385
Hemavathi Vijay <hemavathi.vijaypc@gmail.com>
2022-01-24 22:21:54 +0100

    Update router-app Sidebar component

--- React/router-app/src/App.js
+++ React/router-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -22,8 +22,11 @@ class App extends Component {
               render={(props) => <Products sortby="newest" {...props} />}
             />
             <Route path="/posts/:year?/:month?" component={Posts} />
+            <Redirect from="/messages" to="/posts" />
             <Route path="/admin" component={Dashboard} />
-            <Route path="/" component={Home} />
+            <Route path="/not-found" component={NotFound} />
+            <Route path="/" exact component={Home} />
+            <Redirect to="/not-found" />
           </Switch>
         </div>
       </div>
