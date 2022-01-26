3bcd1e2658625f5805cbf913cad87f4164f581a9
Hemavathi Vijay <hemavathi.vijaypc@gmail.com>
2022-01-27 00:45:29 +0100

    Created input and form. extrated reusable codes. Joi plugin for validation.

--- React/vidly/src/App.js
+++ React/vidly/src/App.js
@@ -1,5 +1,4 @@
 import logo from "./logo.svg";
-import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 import React from "react";
 import Movies from "./components/movies";
@@ -8,6 +7,9 @@ import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import MoviesFrom from "./components/moviesForm";
+import LoginForm from "./components/loginForm";
+import "./App.css";
+
 function App() {
   return (
     <React.Fragment>
@@ -15,6 +17,7 @@ function App() {
       <main className="container">
         <Routes>
           <Route path="/movies/:id" element={<MoviesFrom />} />
+          <Route path="/login" element={<LoginForm />} />
           <Route path="/movies" element={<Movies />}></Route>
           <Route path="/customers" element={<Customers />}></Route>
           <Route path="/rentals" element={<Rentals />}></Route>
